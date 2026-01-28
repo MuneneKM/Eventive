e986d9b2a9e08addb29ae097391823b68230425c
Morris <itsmorriskiburi@gmail.com>
2026-01-28 17:26:33 +0300

    Doctype Framework

---
+++ eventive/networking/doctype/match_suggestion/match_suggestion.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Munene Morris and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Match Suggestion", {
+// 	refresh(frm) {
+
+// 	},
+// });
